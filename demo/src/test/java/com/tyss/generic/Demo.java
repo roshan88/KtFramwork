52a8b5a2d47cbccf5eccd2bd34db1a4adbf80084
TYSS <TYSS@TYSS-TR-001>
2018-03-03 12:55:26 +0530

    sdd1

---
+++ demo/src/test/java/com/tyss/generic/Demo.java
@@ -0,0 +1,11 @@
+package com.tyss.generic;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class Demo {
+	@Test
+	public void test() {
+		Reporter.log("Executing Test.....",true);
+	}
+}
